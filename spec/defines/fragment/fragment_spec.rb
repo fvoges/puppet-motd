5f2090f26124b27bd07631a9fae1101f5c5a384a
Federico Voges <federico@puppetlabs.com>
2016-03-04 12:17:30 +0000

    Add source attribute
    Before this change the motd::fragment defined type only had a content
    attribute and didn't allow to specify a file.
    
    This change modifies the defined type to allow using a source or content
    attribute, similar to how the file type works.
    
    The defined type will raise an error if neither or both attributes are
    provided. In other words, it requires either content or source.

--- spec/defines/fragment/fragment_spec.rb
+++ spec/defines/fragment/fragment_spec.rb
@@ -2,18 +2,8 @@ require 'spec_helper'
 #require 'shared_contexts'
 
 describe 'motd::fragment' do
-  # by default the hiera integration uses hirea data from the shared_contexts.rb file
-  # but basically to mock hiera you first need to add a key/value pair
-  # to the specific context in the spec/shared_contexts.rb file
-  # Note: you can only use a single hiera context per describe/context block
-  # rspec-puppet does not allow you to swap out hiera data on a per test block
-  #include_context :hiera
-
   let(:title) { 'example_name' }
 
-  # below is the facts hash that gives you the ability to mock
-  # facts on a per describe/context block.  If you use a fact in your
-  # manifest you should mock the facts below.
   let(:facts) do
     {
       :osfamily       => 'RedHat',
@@ -24,22 +14,57 @@ describe 'motd::fragment' do
       :path           => '/bin',
     }
   end
-  # below is a list of the resource parameters that you can override.
-  # By default all non-required parameters are commented out,
-  # while all required parameters will require you to add a value
-  let(:params) do
-    {
-      #:content => $title,
-      #:order => 10,
-    }
+
+  context 'with neither content nor source attributes' do
+    it do
+      expect {
+        should compile
+      }.to raise_error(/Has to specify one of content or source/)
+    end
   end
-  # add these two lines in a single test block to enable puppet and hiera debug mode
-  # Puppet::Util::Log.level = :debug
-  # Puppet::Util::Log.newdestination(:console)
-  it do
-    should contain_concat__fragment('motd_fragment_example_name').
-             with({"target"=>"/etc/motd",
-                   "content"=> /example_name/,
-                   "order"=>"10"})
+
+  context 'with both content and source attributes' do
+    let(:params) do
+      {
+        :content => 'test',
+        :source => 'fail',
+      }
+    end
+    it do
+      expect {
+        should compile
+      }.to raise_error(/Has to specify one of content or source/)
+    end
+  end
+
+  context 'with content attribute' do
+    let(:params) do
+      {
+        :content => 'test',
+        #:order => 10,
+      }
+    end
+    it do
+      should contain_concat__fragment('motd_fragment_example_name').
+               with({"target"=>"/etc/motd",
+                     "content"=> /test/,
+                     "order"=>"10"})
+    end
+  end
+
+  context 'with source and order attributes' do
+    let(:params) do
+      {
+        :source => 'test',
+        :order => 20,
+      }
+    end
+
+    it do
+      should contain_concat__fragment('motd_fragment_example_name').
+               with({"target"=>"/etc/motd",
+                     "source"=> /test/,
+                     "order"=>"20"})
+    end
   end
 end
