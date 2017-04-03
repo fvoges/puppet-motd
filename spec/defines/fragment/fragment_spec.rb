a87c7b86b8a28c8e4859b8eb08cd0fd85ae044ee
Federico Voges <federico@puppet.com>
2017-04-03 09:37:53 +0100

    update spec tests to use rspec-puppet-facts

--- spec/defines/fragment/fragment_spec.rb
+++ spec/defines/fragment/fragment_spec.rb
@@ -4,67 +4,71 @@ require 'spec_helper'
 describe 'motd::fragment' do
   let(:title) { 'example_name' }
 
-  let(:facts) do
-    {
-      :osfamily       => 'RedHat',
-      :concat_basedir => '/dfe',
-      :id             => 'dummy',
-      :kernel         => 'Linux',
-      :is_pe          => 'no',
-      :path           => '/bin',
-    }
-  end
+  # use rspec-puppet-facts to generate an iterator to test
+  # each supported platform
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :foo            => 'bar',
+          :concat_basedir => '/dne',
+          :is_pe          => 'no',
+        })
+      end
 
-  context 'with neither content nor source attributes' do
-    it do
-      expect {
-        should compile
-      }.to raise_error(/Has to specify one of content or source/)
-    end
-  end
 
-  context 'with both content and source attributes' do
-    let(:params) do
-      {
-        :content => 'test',
-        :source => 'fail',
-      }
-    end
-    it do
-      expect {
-        should compile
-      }.to raise_error(/Has to specify one of content or source/)
-    end
-  end
+      context 'with neither content nor source attributes' do
+        it do
+          expect {
+            should compile
+          }.to raise_error(/Has to specify one of content or source/)
+        end
+      end
 
-  context 'with content attribute' do
-    let(:params) do
-      {
-        :content => 'test',
-        #:order => 10,
-      }
-    end
-    it do
-      should contain_concat__fragment('motd_fragment_example_name').
-               with({"target"=>"/etc/motd",
-                     "content"=> /test/,
-                     "order"=>"10"})
-    end
-  end
+      context 'with both content and source attributes' do
+        let(:params) do
+          {
+            :content => 'test',
+            :source => 'fail',
+          }
+        end
+        it do
+          expect {
+            should compile
+          }.to raise_error(/Has to specify one of content or source/)
+        end
+      end
 
-  context 'with source and order attributes' do
-    let(:params) do
-      {
-        :source => 'test',
-        :order => 20,
-      }
-    end
+      context 'with content attribute' do
+        let(:params) do
+          {
+            :content => 'test',
+            #:order => 10,
+          }
+        end
+        it do
+          should contain_concat__fragment('motd_fragment_example_name').
+                   with({"target"=>"/etc/motd",
+                         "content"=> /test/,
+                         "order"=>"10"})
+        end
+      end
 
-    it do
-      should contain_concat__fragment('motd_fragment_example_name').
-               with({"target"=>"/etc/motd",
-                     "source"=> /test/,
-                     "order"=>"20"})
+      context 'with source and order attributes' do
+        let(:params) do
+          {
+            :source => 'test',
+            :order => 20,
+          }
+        end
+
+        it do
+          should contain_concat__fragment('motd_fragment_example_name').
+                   with({"target"=>"/etc/motd",
+                         "source"=> /test/,
+                         "order"=>"20"})
+        end
+      end
     end
   end
 end
