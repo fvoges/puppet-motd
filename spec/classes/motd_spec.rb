9e7d2c5080a1d82f72b997a9782b9e1ea7d2751c
Federico Voges <federico@hashicorp.com>
2020-04-13 12:18:26 +0100

    convert to PDK and drop support for Puppet 3.x

--- spec/classes/motd_spec.rb
+++ spec/classes/motd_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-#require 'shared_contexts'
+# require 'shared_contexts'
 
 describe 'motd' do
   # by default the hiera integration uses hirea data from the shared_contexts.rb file
@@ -7,18 +7,15 @@ describe 'motd' do
   # to the specific context in the spec/shared_contexts.rb file
   # Note: you can only use a single hiera context per describe/context block
   # rspec-puppet does not allow you to swap out hiera data on a per test block
-  #include_context :hiera
-
+  # include_context :hiera
 
   # use rspec-puppet-facts to generate an iterator to test
   # each supported platform
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts.merge({
-          :concat_basedir => '/dne',
-          :is_pe          => 'no',
-        })
+        facts.merge(concat_basedir: '/dne',
+                    is_pe: 'no')
       end
 
       # below is a list of the resource parameters that you can override.
@@ -31,26 +28,27 @@ describe 'motd' do
           #:add_puppet_warning => false,
         }
       end
+
       # add these two lines in a single test block to enable puppet and hiera debug mode
       # Puppet::Util::Log.level = :debug
       # Puppet::Util::Log.newdestination(:console)
       it do
-        should contain_concat('/etc/motd').
-                 with({"owner"=>"root",
-                       "group"=>"0",
-                       "mode"=>"0644"})
+        is_expected.to contain_concat('/etc/motd')
+          .with('owner' => 'root',
+                'group' => '0',
+                'mode' => '0644')
       end
-    #  it do
-    #    should not contain_concat__fragment('motd_puppet_warning').
-    #      with_content(/test\.example\.com is managed by Puppet\./)#.
-    #             with({"target"=>"/etc/motd",
-    #                   "order"=>"00"})
-    #  end
+      #  it do
+      #    should not contain_concat__fragment('motd_puppet_warning').
+      #      with_content(/test\.example\.com is managed by Puppet\./)#.
+      #             with({"target"=>"/etc/motd",
+      #                   "order"=>"00"})
+      #  end
       it do
-        should contain_concat__fragment('motd_local').
-                 with({"source"=>"/etc/motd.local",
-                       "target"=>"/etc/motd",
-                       "order"=>"99"})
+        is_expected.to contain_concat__fragment('motd_local')
+          .with('source' => '/etc/motd.local',
+                'target' => '/etc/motd',
+                'order' => '99')
       end
     end
   end
